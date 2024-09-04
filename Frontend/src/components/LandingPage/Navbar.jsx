6f8109ce6ae18c4bfc537669b056d59903d6cb24
Biank123 <biancadepetris1998@gmail.com>
2024-09-04 19:02:24 -0400

    yes

--- Frontend/src/components/LandingPage/Navbar.jsx
+++ Frontend/src/components/LandingPage/Navbar.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Importa los estilos de Font Awesome
-import { useAuth } from '../UserPage/AuthContext'; // Ajusta la ruta si es necesario
+import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import { useAuth } from '../UserPage/AuthContext'; 
 import './Navbar.css';
 
 const Navbar = () => {
