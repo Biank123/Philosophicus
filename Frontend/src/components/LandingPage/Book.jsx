6cbbe4714dbc51a779710d84744d3351835ece9b
Biank123 <biancadepetris1998@gmail.com>
2024-09-23 18:09:23 -0300

    solucionado db

--- Frontend/src/components/LandingPage/Book.jsx
+++ Frontend/src/components/LandingPage/Book.jsx
@@ -28,21 +28,11 @@ const BookProject = () => {
               <div className="corner-fold"></div>
               <div className="page-text w-richtext">
                 <h3><strong>¿Por qué Philosophicus?</strong></h3>
-                <h6>
-                  POR{" "}
-                  <a
-                    href="https://www.poetryfoundation.org/poets/walt-whitman"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Bianca de Petris
-                  </a>
-                </h6>
-                   
-                <p>Philosophicus es una plataforma diseñada para estudiantes que desean mejorar sus habilidades de escritura a través de la creación de ensayos.</p> <br/>
+          
+                <p>Philosophicus es una plataforma diseñada para estudiantes que desean mejorar sus habilidades de escritura a través de la creación de ensayos.</p> <br />
                 <p>La página te guía paso a paso en el proceso de escribir un ensayo,
-                ofreciendo una estructura clara y herramientas útiles para desarrollar y organizar tus ideas,
-                facilitando así la redacción de textos bien fundamentados y coherentes.</p>
+                  ofreciendo una estructura clara y herramientas útiles para desarrollar y organizar tus ideas,
+                  facilitando así la redacción de textos bien fundamentados y coherentes.</p>
               </div>
             </div>
           </div>
@@ -65,26 +55,14 @@ const BookProject = () => {
           <div className="layer-text right">
             <div className="page-right-2">
               <div className="page-text w-richtext">
-                <h3><strong>A Glimpse</strong></h3>
-                <h6>
-                  BY{" "}
-                  <a
-                    href="https://www.poetryfoundation.org/poets/walt-whitman"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    WALT WHITMAN
-                  </a>
-                </h6>
-                <p>A glimpse through an interstice caught,</p>
+                <h3><strong>La importancia de la filosofía</strong></h3>
                 <p>
-                  Of a crowd of workmen and drivers in a bar-room around the
-                  stove late of a winter night, and I unremark’d seated in a
-                  corner,
+                  La filosofía nos invita a cuestionar nuestras creencias, examinar el mundo con una mirada crítica y reflexiva, y buscar respuestas a las preguntas fundamentales sobre la existencia, la moralidad y la naturaleza del conocimiento.
                 </p>
-                <p>Of a youth who loves me and whom I love, silently approaching and seating himself near, that he may hold me by the hand,</p>
-                <p>A long while amid the noises of coming and going, of drinking and oath and smutty jest,</p>
-                <p>There we two, content, happy in being together, speaking little, perhaps not a word.</p>
+                <br />
+                <p>
+                  En Philosophicus, te adentrarás en este vasto mundo de ideas, desarrollando no solo habilidades de escritura, sino también el pensamiento crítico y la capacidad de argumentación sólida, que son esenciales para cualquier disciplina académica y la vida diaria.
+                </p>
               </div>
             </div>
           </div>
