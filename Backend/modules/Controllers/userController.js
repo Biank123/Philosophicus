2afe11ed51738cf34f8354a2ef0ba3c9ec6d6729
Biank123 <biancadepetris1998@gmail.com>
2024-10-26 12:19:03 -0300

    cambiando bcrypt por bcryptjs

--- Backend/modules/Controllers/userController.js
+++ Backend/modules/Controllers/userController.js
@@ -1,5 +1,5 @@
 const userModel = require('../Models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 // Registro de nuevo usuario
